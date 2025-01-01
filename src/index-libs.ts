847a9613d7cdec95ce9a1945b80757a2ccf02060
Daniel Zdancewicz <inwazzin@gmail.com>
2025-01-01 22:50:12 +0100

    declare

--- src/index-libs.ts
+++ src/index-libs.ts
@@ -2,7 +2,15 @@ import * as d3 from 'd3';
 import LZString from 'lz-string';
 import { default as Mousetrap } from 'mousetrap';
 
-globalThis.trash ??= {};
+declare global {
+  interface Trash {
+    d3: typeof d3;
+    LZString: typeof LZString;
+    Mousetrap: typeof Mousetrap;
+  }
+}
+
+globalThis.trash ??= {} as Trash;
 globalThis.trash.d3 = d3;
 globalThis.trash.LZString = LZString;
 globalThis.trash.Mousetrap = Mousetrap;
