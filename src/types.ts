a780161b2e1717902b946bf2349b0254df780a17
Daniel Zdancewicz <inwazzin@gmail.com>
2025-01-06 15:26:52 +0100

    remove globalthistrash

--- src/types.ts
+++ src/types.ts
@@ -1,15 +1,3 @@
-import type { StatGraph } from './stats-graph.ts';
-
-declare global {
-  interface Trash {
-    StatGraph: typeof StatGraph;
-  }
-
-  var trash: Trash;
-}
-
-globalThis.trash ??= {} as Trash;
-
 export type Stat = {
   name: StatName;
   short_form: StatName;
