abbeac12f5ad80a2693b78ec937cbb7fc8779d5d
Daniel Zdancewicz <inwazzin@gmail.com>
2025-01-09 22:12:25 +0100

    single init

--- src/app.ts
+++ src/app.ts
@@ -1,13 +1,19 @@
+import './styles.css';
+
+import { render } from 'solid-js/web';
+import { Application } from './Application.tsx';
 import { Localization } from './localization.ts';
 import { loadDefaults, startGame } from './saving.ts';
 
 import { renderViews } from './views/register-all.ts';
 
+const main = document.getElementById('app');
+if (main) render(Application, main);
+
 loadDefaults();
 
 await Localization.init();
-
 renderViews();
-Localization.populate();
 
+Localization.populate();
 startGame();
--- src/init.ts
+++
@@ -1,7 +0,0 @@
-import './styles.css';
-
-import { render } from 'solid-js/web';
-import { Application } from './Application.tsx';
-
-const main = document.getElementById('app');
-if (main) render(Application, main);
